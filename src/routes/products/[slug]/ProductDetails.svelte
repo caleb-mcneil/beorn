f5825e853375c57194901a61c0686b5269e46e71
Luke McNeil <lukemcneil61@gmail.com>
2023-05-20 11:00:42 -0400

    Stolen product page (#4)
    * stole
    
    * move extra images below

--- src/routes/products/[slug]/ProductDetails.svelte
+++ src/routes/products/[slug]/ProductDetails.svelte
@@ -5,65 +5,58 @@
 </script>
 
 <section>
-    <div
-        class="mx-auto gap-x-8 px-4 pb-8 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8"
-    >
-        <div class="flex flex-wrap gap-4 mb-4">
-            <img alt="primary" class="basis-1/4" src={product.image} />
-            <div class="basis-1/3 grow">
-                <div class="max-w-[35ch] space-y-2">
-                    <h1 class="text-xl font-bold sm:text-2xl">
-                        {product.title}
-                    </h1>
-                </div>
-                <p class="text-lg font-bold">{product.price}</p>
-                <p class="mt-4 text-gray-500">{product.description}</p>
+    <div class="relative mx-auto max-w-screen-xl px-4 py-8">
+        <div class="grid grid-cols-1 items-start gap-8 md:grid-cols-2">
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-1">
+                <img
+                    alt="primary"
+                    src={product.image}
+                    class="aspect-square w-full rounded-xl object-cover"
+                />
             </div>
-            <div class="basis-1/4 grow-1/2">
-                <table>
-                    {#each [...product.stats] as [key, value]}
-                        <tr>
-                            <td class="py-2 px-4 border border-gray-300"
-                                >{key}</td
-                            >
-                            <td class="py-2 px-4 border border-gray-300"
-                                >{value}</td
-                            >
-                        </tr>
-                    {/each}
-                </table>
+
+            <div>
+                <div class="mt-8 flex justify-between">
+                    <div class="max-w-[35ch] space-y-2">
+                        <h1 class="text-xl font-bold sm:text-2xl">
+                            {product.title}
+                        </h1>
+                    </div>
+
+                    <p class="text-lg font-bold">{product.price}</p>
+                </div>
+
+                <div class="mt-4 mb-6">
+                    <div class="prose max-w-none text-justify">
+                        <p>{product.description}</p>
+                    </div>
+                </div>
+                <div>
+                    <table>
+                        {#each [...product.stats] as [key, value]}
+                            <tr>
+                                <td class="border px-4 py-2 text-gray-700"
+                                    >{key}</td
+                                >
+                                <td class="border px-4 py-2 text-gray-700"
+                                    >{value}</td
+                                >
+                            </tr>
+                        {/each}
+                    </table>
+                </div>
             </div>
         </div>
-
-        <div class="container mx-auto px-5 py-2 mt-8">
-            <div class="-m-1 flex flex-wrap md:-m-2">
-                <div class="flex sm:1/1 md:w-1/2 flex-wrap">
-                    {#each product.images as image, index}
-                        {#if index % 2 == 0}
-                            <div class="w-full p-1 md:p-2">
-                                <img
-                                    alt="additional"
-                                    class="block h-full w-full rounded-sm object-cover object-center"
-                                    src={image}
-                                />
-                            </div>
-                        {/if}
-                    {/each}
-                </div>
-                <div class="flex sm:1/1 md:w-1/2 flex-wrap">
-                    {#each product.images as image, index}
-                        {#if index % 2 == 1}
-                            <div class="w-full p-1 md:p-2">
-                                <img
-                                    alt="additional"
-                                    class="block h-full w-full rounded-sm object-cover object-center"
-                                    src={image}
-                                />
-                            </div>
-                        {/if}
-                    {/each}
-                </div>
-            </div>
+        <div
+            class="grid gap-4 mt-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+        >
+            {#each product.images as image}
+                <img
+                    alt="additional"
+                    src={image}
+                    class="aspect-square w-full rounded-xl object-cover"
+                />
+            {/each}
         </div>
     </div>
 </section>
