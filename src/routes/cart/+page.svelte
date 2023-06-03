e87d17c43d892d160831fb68a4fab5cf45016b2a
Adam <adamwmcneil@gmail.com>
2023-06-03 10:38:39 -0500

    build is not working correctly

---
+++ src/routes/cart/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    // function printCart() {
+    //     console.log(localStorage)
+    // }
+
+    function loadLocalStorage() {
+        // let cart = new Map();
+        // for (let i = 0; i < localStorage.length; i++) {
+        //     let key = localStorage.key(i);
+        //     cart.set(key, localStorage.getItem(key))
+        // }
+        // return cart;
+        if (localStorage != null) {
+            return Object.entries(localStorage);
+        }
+        else {
+            return [];
+        }
+    }
+</script>
+
+<section>
+    <h1>
+        Cart
+    </h1>
+    {#each loadLocalStorage() as [key, value]}
+        <p>
+            {key}{value}
+        </p>
+    {/each}
+</section>
