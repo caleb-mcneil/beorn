a019e0d9672aea982bf046894184b1c92d461231
caleb-mcneil <132161455+caleb-mcneil@users.noreply.github.com>
2023-09-08 10:48:10 -0500

    Update +layout.svelte
    call/text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -60,7 +60,7 @@
                     <li>
                         <a
                             class="inline-block no-underline hover:text-black hover:underline py-2 px-4"
-                            href="tel:314-514-5969">314-514-5969</a 
+                            href="tel:314-514-5969">314-514-5969 Call/Text</a 
                         >
                     </li>
                     <li>|</li>
@@ -117,7 +117,7 @@
                         <li>
                             <a
                                 class="inline-block no-underline hover:text-black hover:underline py-2 px-4"
-                                href="tel:314-514-5969">314-514-5969</a 
+                                href="tel:314-514-5969">314-514-5969 Call/Text</a 
                             >
                         </li>
                     </ul>
