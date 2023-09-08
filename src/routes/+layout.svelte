f50bea5307becd31d14b872c072ada1792870e6a
caleb-mcneil <132161455+caleb-mcneil@users.noreply.github.com>
2023-09-08 15:09:36 -0500

    Update +layout.svelte
    comment out cart

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -70,13 +70,13 @@
                             href="#about-us">About Us</a
                         >
                     </li>
-                    //<li>|</li>
-                    //<li>
-                    //    <a
-                    //        class="inline-block no-underline hover:text-black hover:underline py-2 px-4"
-                    //        href="/cart">Cart</a
-                    //    >
-                    //</li>
+                    <!--<li>|</li>
+                    <li>
+                        <a
+                            class="inline-block no-underline hover:text-black hover:underline py-2 px-4"
+                            href="/cart">Cart</a
+                        >
+                    </li> -->
                 </ul>
             </nav>
         </div>
