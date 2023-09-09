9c85cdb707b3d9bca526225774f9dba699b7e2df
Luke McNeil <lukemcneil61@gmail.com>
2023-09-09 00:24:30 -0500

    make hero image look good on both mobile and web

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,42 @@
 <script lang="ts">
     import { products } from "$lib/products";
     import ProductLink from "./ProductLink.svelte";
+    const images = import.meta.glob("$lib/images/lineup/*.webp", {
+        import: "default",
+        eager: true,
+    });
+    const lineupImage: any = images[`/src/lib/images/lineup/002.webp`];
 </script>
 
 <title>Beorn LLC</title>
 
 <!-- hero image -->
 <section
-    class="w-full mx-auto flex pt-12 md:pt-0 md:items-center bg-amber-200"
-    style="max-width:1600px; height: 16rem;"
+    class="w-full mx-auto flex py-6 md:py-6 md:items-center bg-amber-200"
+    style="max-width:1600px;"
 >
-    <div class="container mx-auto">
+    <div class="flex flex-wrap px-6">
         <div
-            class="flex flex-col w-full lg:w-1/2 justify-center items-start px-6 tracking-wide"
+            class="flex flex-col w-full md:w-1/2 justify-center items-center tracking-wide"
         >
-            <h1 class="text-black text-3xl my-4">Excavators and More</h1>
+            <h1 class="text-black text-3xl">Excavators and More</h1>
             <div>
                 <a
-                    class="text-2xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black"
+                    class="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black"
                     href="#products">Products</a
                 >
-                <span class="text-2xl">|</span>
+                <span class="text-xl">|</span>
                 <a
-                    class="text-2xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black"
+                    class="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black"
                     href="videos">See them in action</a
                 >
             </div>
         </div>
+        <img
+            class="py-6 md:w-1/2"
+            src={lineupImage}
+            alt={"line up of equipment"}
+        />
     </div>
 </section>
 
