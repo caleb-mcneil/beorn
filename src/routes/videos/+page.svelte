82be15298e51720f4dc4d7ad4649f0b83f7d5787
caleb-mcneil <132161455+caleb-mcneil@users.noreply.github.com>
2023-09-09 00:58:21 -0500

    Update +page.svelte

--- src/routes/videos/+page.svelte
+++ src/routes/videos/+page.svelte
@@ -19,13 +19,9 @@
 
         <Video
             title="Title"
-            link="https://www.youtube.com/embed/zrkK7-ysMuY?si=CqMJse7WctG6t6Fq?origin=http://example.com"
+            link="https://www.youtube.com/embed/zrkK7-ysMuY?si=80bgkMMW7rcTRNa_"
         />
 
-        <Video
-            title="Title"
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/zrkK7-ysMuY?si=80bgkMMW7rcTRNa_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        />
         <Video
             title="Title"
             link="https://www.youtube.com/embed/as3qWsbYGWs?origin=http://example.com"
