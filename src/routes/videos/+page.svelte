5b8a975b38b8cde7f69cdf6d9a38f6517c368904
caleb-mcneil <132161455+caleb-mcneil@users.noreply.github.com>
2023-09-09 00:27:18 -0500

    Update +page.svelte

--- src/routes/videos/+page.svelte
+++ src/routes/videos/+page.svelte
@@ -9,7 +9,7 @@
     <div class="container mx-auto flex flex-wrap pb-12">
         <Video
             title="SE08 Demo"
-            link="https://youtu.be/zrkK7-ysMuY"
+            link="https://www.youtube.com/watch?v=zrkK7-ysMuY&ab_channel=BeornLLC"
         />
 
         <Video
