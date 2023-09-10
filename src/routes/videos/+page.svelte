74cf39944e89dd2e7d600f7a75a220e51008d23d
Luke McNeil <lukemcneil61@gmail.com>
2023-09-10 13:44:45 -0500

    add videos to page of equipment

--- src/routes/videos/+page.svelte
+++ src/routes/videos/+page.svelte
@@ -1,51 +1,16 @@
 <script lang="ts">
+    import { products } from "$lib/products";
     import Video from "./Video.svelte";
-    // TODO: convert this to have a data list and iterating over them
 </script>
 
 <title>Beorn LLC: Videos</title>
 
 <section id="products" class="bg-white">
     <div class="container mx-auto flex flex-wrap pb-12">
-
-        <Video
-            title="SE17P Demo"
-            link="https://www.youtube.com/embed/Kw18-9yWb_0?si=Z9itJJKqPkRRw_Oc"
-        />
-
-         <Video
-            title="SE10P Demo"
-            link="https://www.youtube.com/embed/jHUwegq497c?si=NMfIEDtJ_wjliFWM"
-        />
-
-        <Video
-            title="SE08 Demo"
-            link="https://www.youtube.com/embed/zrkK7-ysMuY?si=XovyD4faO5eKYJaG"
-        />
-
-        <Video
-            title="DC08 Demo"
-            link="https://www.youtube.com/embed/kUHqh6LybvQ?si=GVtYuvxDtomOc_sD"
-        />
-
-        <Video
-            title="SE17 Demo"
-            link="https://www.youtube.com/embed/ZqbSqlKf0xc?si=3OSmhBngmeckKGzK"
-        />
-
-        <Video
-            title="SE10 Demo"
-            link="https://www.youtube.com/embed/CsYZGTKnmhY?si=t3I5sVKOVZ7us5d9"
-        />
-
-        <Video
-            title="Quick Connect Coupler Demo"
-            link=""
-        />
-
-        <Video
-            title="Auger Demo"
-            link=""
-        />
+        {#each products as product}
+            {#if product.video}
+                <Video title="{product.title} Demo" link={product.video} />
+            {/if}
+        {/each}
     </div>
 </section>
